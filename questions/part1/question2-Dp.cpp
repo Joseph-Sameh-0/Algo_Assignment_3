c4c7906cd7d6bb71222fb09dff55d5a9cd58fa89
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-12-29 00:45:31 +0200

    .

---
+++ questions/part1/question2-Dp.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long fib[100000 + 5];
+long long mod = 1e9 + 7;
+
+long long containWM(string s)
+{
+    for (char c : s)
+    {
+        if (c == 'w' || c == 'm')
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void checkForCharOccurrence(const string &s, const char &ch, long long &sum)
+{
+    long long answer = 0;
+    for (char c : s)
+    {
+        if (c == ch)
+        {
+            answer++;
+        }
+        else if (answer > 0)
+        {
+            sum = (sum * fib[answer]) % mod;
+            answer = 0;
+        }
+    }
+    if (answer > 0)
+    {
+        sum = (sum * fib[answer]) % mod;
+        answer = 0;
+    }
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    fib[1] = 1;
+    fib[2] = 2;
+
+    for (int i = 3; i <= n; i++)
+    {
+        fib[i] = (fib[i - 1] + fib[i - 2]) % mod;
+    }
+
+    if (containWM(s))
+    {
+        cout << "0";
+    }
+
+    else
+    {
+        long long sum = 1;
+
+        checkForCharOccurrence(s, 'n', sum);
+
+        checkForCharOccurrence(s, 'u', sum);
+
+        cout << sum;
+    }
+    return 0;
+}
